381a03c0dbe0aad4a7c9d00e3cd60c3a046a74d7
23JW4346 <23JW4346@barton.ac.uk>
2024-12-04 06:11:56 +0000

    day 4

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Web;
 
 namespace Day_4
 {
@@ -21,13 +17,13 @@ namespace Day_4
         static int SOl1(string[] lines)
         {
             int answer = 0;
-            for(int i = 0; i < lines.Length; i++)
+            for (int i = 0; i < lines.Length; i++)
             {
-                for(int j = 0; j < lines[i].Length; j++)
+                for (int j = 0; j < lines[i].Length; j++)
                 {
                     if (lines[i][j] == 'X')
                     {
-                        answer += CheckAround(lines, j,i );
+                        answer += CheckAround(lines, j, i);
                     }
                 }
             }
@@ -37,38 +33,14 @@ namespace Day_4
         static int CheckAround(string[] lines, int x, int y)
         {
             List<string> maybes = new List<string>();
-            if(x < lines[y].Length-3)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y][x+1].ToString() + lines[y][x+2].ToString() + lines[y][x + 3].ToString());
-            }
-            if(x > 2)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y][x - 1].ToString() + lines[y][x - 2].ToString() + lines[y][x - 3].ToString());
-            }
-            if(y> 2)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y - 1][x].ToString() + lines[y - 2][x].ToString() + lines[y - 3][x].ToString());
-            }
-            if(y < lines.Length-3)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y + 1][x].ToString() + lines[y+ 2][x].ToString() + lines[y + 3][x].ToString());
-            }
-            if(x > 2 && y > 2)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y - 1][x -1].ToString() + lines[y - 2][x-2].ToString() + lines[y - 3][x-3].ToString());
-            }
-            if(x < lines[y].Length-3 && y > 2)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y - 1][x + 1].ToString() + lines[y - 2][x + 2].ToString() + lines[y - 3][x + 3].ToString());
-            }
-            if(x > 2 && y < lines.Length - 3)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y + 1][x - 1].ToString() + lines[y + 2][x - 2].ToString() + lines[y + 3][x - 3].ToString());
-            }
-            if(x < lines[y].Length-3 && y < lines.Length-3)
-            {
-                maybes.Add(lines[y][x].ToString() + lines[y + 1][x + 1].ToString() + lines[y + 2][x + 2].ToString() + lines[y + 3][x + 3].ToString());
-            }
+            if (x < lines[y].Length - 3) maybes.Add(lines[y][x].ToString() + lines[y][x + 1].ToString() + lines[y][x + 2].ToString() + lines[y][x + 3].ToString());
+            if (x > 2) maybes.Add(lines[y][x].ToString() + lines[y][x - 1].ToString() + lines[y][x - 2].ToString() + lines[y][x - 3].ToString());
+            if (y > 2) maybes.Add(lines[y][x].ToString() + lines[y - 1][x].ToString() + lines[y - 2][x].ToString() + lines[y - 3][x].ToString());
+            if (y < lines.Length - 3) maybes.Add(lines[y][x].ToString() + lines[y + 1][x].ToString() + lines[y + 2][x].ToString() + lines[y + 3][x].ToString());
+            if (x > 2 && y > 2) maybes.Add(lines[y][x].ToString() + lines[y - 1][x - 1].ToString() + lines[y - 2][x - 2].ToString() + lines[y - 3][x - 3].ToString());
+            if (x < lines[y].Length - 3 && y > 2) maybes.Add(lines[y][x].ToString() + lines[y - 1][x + 1].ToString() + lines[y - 2][x + 2].ToString() + lines[y - 3][x + 3].ToString());
+            if (x > 2 && y < lines.Length - 3) maybes.Add(lines[y][x].ToString() + lines[y + 1][x - 1].ToString() + lines[y + 2][x - 2].ToString() + lines[y + 3][x - 3].ToString());
+            if (x < lines[y].Length - 3 && y < lines.Length - 3) maybes.Add(lines[y][x].ToString() + lines[y + 1][x + 1].ToString() + lines[y + 2][x + 2].ToString() + lines[y + 3][x + 3].ToString());
             int temp = 0;
             foreach (string s in maybes)
             {
@@ -80,19 +52,16 @@ namespace Day_4
         static int SOl2(string[] lines)
         {
             int answer = 0;
-            for(int i =0; i < lines.Length; i++)
+            for (int i = 1; i < lines.Length - 1; i++)
             {
-                for(int j = 0;j < lines[i].Length; j++)
+                for (int j = 1; j < lines[i].Length - 1; j++)
                 {
                     if (lines[i][j] == 'A')
                     {
-                        if (i > 0 && j > 0 && i < lines.Length - 1 && j < lines[i].Length - 1)
-                        {
-                            string lefttoright = lines[i - 1][j - 1].ToString() + lines[i][j].ToString() + lines[i + 1][j + 1].ToString();
-                            string righttoleft = lines[i - 1][j + 1].ToString() + lines[i][j].ToString() + lines[i + 1][j - 1].ToString();
-                            if ((Regex.IsMatch(lefttoright, "MAS")||Regex.IsMatch(lefttoright, "SAM")) 
-                               && (Regex.IsMatch(righttoleft, "MAS")|Regex.IsMatch(righttoleft, "SAM"))) answer++;
-                        }
+                        string lefttoright = lines[i - 1][j - 1].ToString() + lines[i][j].ToString() + lines[i + 1][j + 1].ToString();
+                        string righttoleft = lines[i - 1][j + 1].ToString() + lines[i][j].ToString() + lines[i + 1][j - 1].ToString();
+                        if ((Regex.IsMatch(lefttoright, "MAS") || Regex.IsMatch(lefttoright, "SAM"))
+                           && (Regex.IsMatch(righttoleft, "MAS") | Regex.IsMatch(righttoleft, "SAM"))) answer++;
                     }
                 }
             }
