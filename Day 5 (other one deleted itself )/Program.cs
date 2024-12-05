3f94df2102389898953c5d056d9ecfdfa1e8b047
23JW4346 <23JW4346@barton.ac.uk>
2024-12-05 17:36:39 +0000

    Day 5

---
+++ Day 5 (other one deleted itself )/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web;
+using System.CodeDom.Compiler;
+using System.Runtime.Remoting.Messaging;
+
+namespace Day_5__other_one_deleted_itself__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            List<string> updates = new List<string>();
+            List<string> rules = new List<string>();
+            foreach (string line in lines)
+            {
+                if (line.Contains("|"))
+                {
+                    rules.Add(line);
+                }
+                else if(line.Contains(",")) 
+                {
+                    updates.Add(line);
+                }
+            }
+            Console.WriteLine($"Part 1: {SOL1(updates, rules)}\nPart 2: {SOL2(updates, rules)}");
+            Console.ReadKey();
+        }
+
+        static int SOL1(List<string> updates, List<string> rules) 
+        {
+            int answer = 0;
+            foreach(string update in updates)
+            {
+                    if (CheckUpdate(update, rules.ToArray()))
+                    {
+                        string[] temp = update.Split(',');
+                        answer += int.Parse(temp[temp.Length/2]);
+                    } 
+            }
+            return answer;
+        }
+
+        static bool CheckUpdate(string update, string[] rules)
+        {
+            foreach (string rule in rules)
+            {
+                (string a, string b) = (rule.Substring(0, 2), rule.Substring(3, 2));
+                string[] temp = update.Split(',');
+                if (temp.Contains(a) && temp.Contains(b))
+                {
+                    if (temp.ToList().IndexOf(a) > temp.ToList().IndexOf(b)) return false;
+                }
+            }
+            return true;
+        }
+
+        static int SOL2(List<string> updates, List<string> rules)
+        {
+            int answer = 0;
+            foreach (string update in updates)
+            {
+                if(!CheckUpdate(update, rules.ToArray()))
+                {
+                    string[] temp = update.Split(',');
+                    List<int> updater = new List<int>();
+                    foreach(string s in temp)
+                    {
+                        updater.Add(int.Parse(s));
+                    }
+                    updater.Sort((a, b) =>
+                    {
+                        foreach (string rule in rules)
+                        {
+                            (int a, int b) order = (int.Parse(rule.Substring(0, 2)), int.Parse(rule.Substring(3, 2)));
+                            if (order == (a, b) || order == (b, a))
+                            {
+                                return a == order.a ? -1 : 1;
+                            }
+                        }
+                        return 0;
+                    });
+                    answer += updater[updater.Count / 2];
+                }
+            }
+            return answer;
+        }
+        
+    }
+}
