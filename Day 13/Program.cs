0b06705e68209369d22d6238a42754e01940107e
23JW4346 <23JW4346@barton.ac.uk>
2024-12-13 12:17:37 +0000

    Day 13

--- Day 13/Program.cs
+++ Day 13/Program.cs
@@ -9,13 +9,29 @@ namespace Day_13
 
         public struct clawMachine
         {
-            public button a, b;
+            public Matrix m;
             public long xans, yans;
         }
-
-        public struct button
+        public class Matrix
         {
-            public int x, y;
+            public long a, b, c, d;
+            public Matrix((int x, int y) ina, (int x, int y) inb)
+            {
+                a = ina.x;
+                c = ina.y;
+                b = inb.x;
+                d = inb.y;
+            }
+
+            public long GetDeterminent() => a * d - b * c;
+
+            public (long, long) Solve((long e, long f) answer)
+            {
+                (long x, long y) variables = (0, 0);
+                variables.x = (d * answer.e - b * answer.f) / GetDeterminent();
+                variables.y = (-c * answer.e + a * answer.f) / GetDeterminent();
+                return variables;
+            }
         }
 
         static void Main(string[] args)
@@ -29,7 +45,8 @@ namespace Day_13
         {
             string[] lines = File.ReadAllLines("input.txt");
             List<clawMachine> claw = new List<clawMachine>();
-            button buta = new button(), butb = new button();
+            (int x, int y) left = (0,0), right = (0,0);
+            
             int i = 0;
             foreach (string line in lines)
             {
@@ -37,16 +54,16 @@ namespace Day_13
                 {
                     string temp = line.Substring(10);
                     string[] splits = temp.Trim().Split(',');
-                    buta.x = int.Parse(splits[0].Trim().Substring(1));
-                    buta.y = int.Parse(splits[1].Trim().Substring(1));
+                    left.x = int.Parse(splits[0].Trim().Substring(1));
+                    left.y = int.Parse(splits[1].Trim().Substring(1));
                     i++;
                 }
                 else if (i == 1)
                 {
                     string temp = line.Substring(10);
                     string[] splits = temp.Trim().Split(',');
-                    butb.x = int.Parse((splits[0].Trim().Substring(1)));
-                    butb.y = int.Parse(splits[1].Trim().Substring(1));
+                    right.x = int.Parse((splits[0].Trim().Substring(1)));
+                    right.y = int.Parse(splits[1].Trim().Substring(1));
                     i++;
                 }
                 else if (i == 2)
@@ -56,8 +73,7 @@ namespace Day_13
                     clawMachine myClaw;
                     myClaw.xans = int.Parse(splits[0].Trim().Substring(1));
                     myClaw.yans = int.Parse(splits[1].Trim().Substring(2));
-                    myClaw.a = buta;
-                    myClaw.b = butb;
+                    myClaw.m = new Matrix(left, right);
                     claw.Add(myClaw);
                     i++;
                 }
@@ -80,37 +96,14 @@ namespace Day_13
             return coins;
         }
 
-        public class Matrix
-        {
-            private long a, b, c, d;
-            public Matrix((int x, int y) ina, (int x, int y) inb)
-            {
-                a = ina.x;
-                c = ina.y;
-                b = inb.x;
-                d = inb.y;
-            }
-
-            public long GetDeterminent() => a * d - b * c;
-
-            public (long, long) Solve((long e, long f) answer)
-            {
-                (long x, long y) variables = (0, 0);
-                variables.x = (d * answer.e - b * answer.f) / GetDeterminent();
-                variables.y = (-c * answer.e + a * answer.f) / GetDeterminent();
-                return variables;
-            }
-        }
-
         static (long, long) GetToXY(clawMachine machine)
         {
             long acount, bcount;
-            Matrix equation = new Matrix((machine.a.x, machine.a.y), (machine.b.x, machine.b.y));
-            if (equation.GetDeterminent() != 0)
+            if (machine.m.GetDeterminent() != 0)
             {
-                acount = equation.Solve((machine.xans, machine.yans)).Item1;
-                bcount = equation.Solve((machine.xans, machine.yans)).Item2;
-                if ((acount * machine.a.x + bcount * machine.b.x, acount * machine.a.y + bcount * machine.b.y) == (machine.xans, machine.yans))
+                acount = machine.m.Solve((machine.xans, machine.yans)).Item1;
+                bcount = machine.m.Solve((machine.xans, machine.yans)).Item2;
+                if ((acount * machine.m.a + bcount * machine.m.b, acount * machine.m.c + bcount * machine.m.d) == (machine.xans, machine.yans))
                 {
                     return (acount, bcount);
                 }
@@ -125,8 +118,7 @@ namespace Day_13
             foreach (clawMachine machine in machines)
             {
                 clawMachine myClaw;
-                myClaw.a = machine.a;
-                myClaw.b = machine.b;
+                myClaw.m = machine.m;
                 myClaw.xans = machine.xans + offset;
                 myClaw.yans = machine.yans + offset;
                 (long a, long b) temp = GetToXY(myClaw);
