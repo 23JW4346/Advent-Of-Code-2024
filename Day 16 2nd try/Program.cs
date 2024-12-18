118a2c8d33ccb60c9d2a82a58ca2bff873826005
23JW4346 <23JW4346@barton.ac.uk>
2024-12-18 20:37:54 +0000

    :(

--- Day 16 2nd try/Program.cs
+++ Day 16 2nd try/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Day_16_2nd_try
 {
@@ -25,7 +22,7 @@ namespace Day_16_2nd_try
 
         static int SOL1(char[,] maze)
         {
-            ((int,int) start, (int,int) end) = FindStartEnd(maze);
+            ((int, int) start, (int, int) end) = FindStartEnd(maze);
             int result = BFS(maze, start, end);
             return result;
         }
@@ -37,72 +34,60 @@ namespace Day_16_2nd_try
             for (int i = 0; i < lines.Length; i++)
                 for (int j = 0; j < lines[i].Length; j++)
                     maze[j, i] = lines[i][j];
-            
+
             return maze;
         }
 
-        static int BFS(char[,] map, (int x,int y) startPos, (int x,int y) endPos)
+        static int BFS(char[,] map, (int x, int y) startPos, (int x, int y) endPos)
         {
-            (int x, int y)[] dxy = {(1, 0), (0, 1), (-1,0) ,(0, -1) };
-            int[,] costs = new int[map.GetLength(0),map.GetLength(1)];
-            for(int x = 0; x < costs.GetLength(0); x++)
-                for(int y = 0; y < costs.GetLength(1); y++)
-                    costs[x, y] = int.MaxValue;
-            bool[,,] visited = new bool[map.GetLength(0),map.GetLength(1), 4];
-            Queue<(int x, int y, Direction,int cost)> myQueue = new Queue<(int x, int y, Direction, int cost)>();
-            myQueue.Enqueue((startPos.x, startPos.y, Direction.right ,0));
+            (int x, int y)[] dxy = { (1, 0), (0, 1), (-1, 0), (0, -1) };
+            int[,,] costs = new int[map.GetLength(0), map.GetLength(1), 4];
+            for (int x = 0; x < costs.GetLength(0); x++)
+                for (int y = 0; y < costs.GetLength(1); y++)
+                    for(int d = 0; d <  dxy.GetLength(0); d++)  
+                        costs[x, y, d] = int.MaxValue;
+            HashSet<(int, int, Direction)> seen = new HashSet<(int, int, Direction)>();
+            int minval = int.MaxValue;
+            Queue<(int x, int y, Direction, int cost)> myQueue = new Queue<(int x, int y, Direction, int cost)>();
+            myQueue.Enqueue((startPos.x, startPos.y, Direction.right, 0));
             while (myQueue.Count > 0)
             {
                 (int x, int y, Direction dir, int curCost) = myQueue.Dequeue();
-                if (!visited[x, y, (int)dir] && (x, y) != endPos)
+                if (costs[x, y, (int)dir] > curCost)
+                    costs[x, y, (int)dir] = curCost;
+                if ((x, y) == endPos)
+                    minval = costs[x, y, (int)dir];
+                else if (seen.Add((x, y, dir)))
                 {
-                    visited[x, y, (int)dir] = true;
-                    if (costs[x, y] > curCost)
-                        costs[x, y] = curCost;
                     int nx = x + dxy[(int)dir].x;
                     int ny = y + dxy[(int)dir].y;
                     if (IsValidMove(nx, ny, map))
                         myQueue.Enqueue((nx, ny, dir, curCost + 1));
-
                     Direction tempDir = (Direction)(((int)dir + 1) % 4);
                     int tempCost = curCost + 1000;
-                    if (curCost < costs[x, y]) costs[x, y] = tempCost;
-                    else tempCost = costs[x, y] + 1000;
-
                     myQueue.Enqueue((x, y, tempDir, tempCost));
-
                     tempDir = (Direction)(((int)dir + 3) % 4);
-                    tempCost = curCost + 1000;
-                    if (curCost < costs[x, y])
-                        costs[x, y] = tempCost;
-                    else
-                        tempCost = costs[x, y] + 1000;
-
                     myQueue.Enqueue((x, y, tempDir, tempCost));
                 }
-                else
-                    costs[x, y] = curCost < costs[x, y] ? curCost : costs[x, y];
-                
-                
             }
-            return costs[endPos.x, endPos.y];
+            return minval;
         }
 
-        static ((int,int), (int,int)) FindStartEnd(char[,] maze)
+        static ((int, int), (int, int)) FindStartEnd(char[,] maze)
         {
-            (int,int)start = (0, 0), end = (0, 0);
+            (int, int) start = (0, 0), end = (0, 0);
             for (int i = 0; i < maze.GetLength(0); i++)
                 for (int j = 0; j < maze.GetLength(1); j++)
                     if (maze[j, i] == 'E')
                         end = (j, i);
                     else if (maze[j, i] == 'S')
                         start = (j, i);
-                    
+
             return (start, end);
         }
 
 
         static bool IsValidMove(int x, int y, char[,] maze) => x >= 0 && y >= 0 && x < maze.GetLength(0) && y < maze.GetLength(1) && maze[x, y] != '#';
-        
+
     }
 }
--- Day 16/Program.cs
+++ Day 16/Program.cs
@@ -1,23 +1,32 @@
 ﻿using System;
-using System.Collections;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 
-namespace Day_16
+namespace Day_16_2nd_try
 {
     internal class Program
     {
 
+        public enum Direction
+        {
+            right, down, left, up
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine($"Part 1:");
-            Console.WriteLine($"Test 1: {SOL1(GetPuzzleInput("test1.txt"))}");
-            Console.WriteLine($"Test 2: {SOL1(GetPuzzleInput("test2.txt"))}");
-            Console.WriteLine($"Actual: {SOL1(GetPuzzleInput("input.txt"))}");
+            Console.WriteLine($"Test1: {SOL1(GetPuzzleInput("test1.txt"))}");
+            Console.WriteLine($"Test2: {SOL1(GetPuzzleInput("test2.txt"))}");
+            Console.WriteLine($"Input: {SOL1(GetPuzzleInput("input.txt"))}");
             Console.ReadKey();
+        }
+
+        static int SOL1(char[,] maze)
+        {
+            ((int, int) start, (int, int) end) = FindStartEnd(maze);
+            int result = BFS(maze, start, end);
+            return result;
         }
 
         static char[,] GetPuzzleInput(string filename)
@@ -25,128 +34,81 @@ namespace Day_16
             string[] lines = File.ReadAllLines(filename);
             char[,] maze = new char[lines[0].Length, lines.Length];
             for (int i = 0; i < lines.Length; i++)
-            {
                 for (int j = 0; j < lines[i].Length; j++)
-                {
-                    maze[j,i] = lines[i][j];
-                }
-            }
+                    maze[j, i] = lines[i][j];
+
             return maze;
         }
 
-        static long SOL1(char[,] maze)
+        static int BFS(char[,] map, (int x, int y) startPos, (int x, int y) endPos)
         {
-            ((int x,int y) start, (int x, int y) end) = FindStartAndEnd(maze);
-            int mazetraveled = BFS(maze, start, end);
-            return mazetraveled;
+            (int x, int y)[] dxy = { (1, 0), (0, 1), (-1, 0), (0, -1) };
+            int[,] costs = new int[map.GetLength(0), map.GetLength(1)];
+            for (int x = 0; x < costs.GetLength(0); x++)
+                for (int y = 0; y < costs.GetLength(1); y++)
+                    costs[x, y] = int.MaxValue;
+            bool[,,] visited = new bool[map.GetLength(0), map.GetLength(1), 4];
+            Queue<(int x, int y, Direction, int cost)> myQueue = new Queue<(int x, int y, Direction, int cost)>();
+            myQueue.Enqueue((startPos.x, startPos.y, Direction.right, 0));
+            while (myQueue.Count > 0)
+            {
+                (int x, int y, Direction dir, int curCost) = myQueue.Dequeue();
+                if ((x, y) == endPos)
+                {
+                    costs[x, y] = costs[x, y] > curCost ? curCost : costs[x, y];
+                }
+                else if (!visited[x, y, (int)dir])
+                {
+                    visited[x, y, (int)dir] = true;
+                    if (costs[x, y] > curCost)
+                        costs[x, y] = curCost;
+                    int nx = x + dxy[(int)dir].x;
+                    int ny = y + dxy[(int)dir].y;
+                    if (IsValidMove(nx, ny, map))
+                        myQueue.Enqueue((nx, ny, dir, curCost + 1));
+
+                    Direction tempDir = (Direction)(((int)dir + 1) % 4);
+                    nx = x + dxy[(int)tempDir].x;
+                    ny = y + dxy[(int)tempDir].y;
+                    int tempCost = curCost + 1001;
+                    costs[nx, ny] = tempCost;
+                    if (costs[nx, ny] > curCost)
+                        costs[nx, ny] = tempCost;
+                    else
+                        tempCost = costs[nx, ny] + 1001;
+                    if (IsValidMove(nx, ny, map))
+                        myQueue.Enqueue((nx, ny, tempDir, tempCost));
+
+                    tempDir = (Direction)(((int)dir + 3) % 4);
+                    nx = x + dxy[(int)tempDir].x;
+                    ny = y + dxy[(int)tempDir].y;
+                    tempCost = curCost + 1001;
+                    if (costs[nx, ny] > curCost)
+                        costs[nx, ny] = tempCost;
+                    else
+                        tempCost = costs[nx, ny] + 1001;
+                    if (IsValidMove(nx, ny, map))
+                        myQueue.Enqueue((nx, ny, tempDir, tempCost));
+                }
+            }
+            return costs[endPos.x, endPos.y];
         }
 
-        static void Print(char[,] maze, (int x, int y) point)
+        static ((int, int), (int, int)) FindStartEnd(char[,] maze)
         {
+            (int, int) start = (0, 0), end = (0, 0);
             for (int i = 0; i < maze.GetLength(0); i++)
-            {
                 for (int j = 0; j < maze.GetLength(1); j++)
-                {
-                    if ((j, i) == point) Console.Write('X');
-                    else Console.Write(maze[j,i]);
-                }
-                Console.WriteLine();
-            }
-        }
+                    if (maze[j, i] == 'E')
+                        end = (j, i);
+                    else if (maze[j, i] == 'S')
+                        start = (j, i);
 
-        static ((int, int), (int, int)) FindStartAndEnd(char[,] maze)
-        {
-            (int x, int y) start = (0, 0), end = (0, 0);
-            for (int i = 0; i < maze.GetLength(0); i++)
-            {
-                for (int j = 0; j < maze.GetLength(1); j++)
-                {
-                    if (maze[i, j] == 'S') // Start point
-                    {
-                        start = (i, j);
-                    }
-                    else if (maze[i, j] == 'E') // End point
-                    {
-                        start = (i, j);
-                    }
-                }
-            }
             return (start, end);
         }
 
-        static int BFS(char[,] maze, (int x, int y) start, (int x, int y) end)
-        {
-            int rows = maze.GetLength(0);
-            int cols = maze.GetLength(1);
 
-            (int, int)[] directions = { (1, 0), (0, -1), (-1, 0), (0, -1) };
-            // Distance matrix to keep track of the shortest distance from the start
-            int[,] distance = new int[rows, cols];
-            for(int x = 0; x < rows; x++)
-            {
-                for(int y = 0; y < cols; y++)
-                {
-                    distance[x, y] = int.MaxValue;
-                }
-            }
-            bool[,,] visited = new bool[rows, cols, directions.Length]; // To track visited cells
-            (int, int)[,] parent = new (int, int)[rows, cols]; // For path reconstruction
+        static bool IsValidMove(int x, int y, char[,] maze) => x >= 0 && y >= 0 && x < maze.GetLength(0) && y < maze.GetLength(1) && maze[x, y] != '#';
 
-            Queue<((int, int),(int ,int)) > queue = new Queue<((int, int), (int, int))>();
-            queue.Enqueue((start, directions[0]));
-            visited[start.x, start.y,3] = true;
-            distance[start.x, start.y] = 0;
-
-            // BFS traversal
-            while (queue.Count > 0)
-            {
-                ((int, int) current, (int x, int y) direction) = queue.Dequeue();
-                // If we reach the end point
-                if (current == end)
-                {
-                    PrintPath(parent, end.x, end.y);
-                }
-                int x = current.Item1;
-                int y = current.Item2;
-
-                // Explore Neighbours
-                for (int i = 0; i < 4; i += 1)
-                {
-                    (int x, int y) d = directions[(Array.IndexOf(directions, direction) + 1) % 4];
-                    int nx = x + d.x;
-                    int ny = y + d.y;
-                    if(d == (-direction.x, -direction.y))
-                    // check if new position isn't in the wall and not visited
-                    if (maze[nx, ny] != '#' && !visited[nx, ny,Array.IndexOf(directions,d)])
-                    {
-                        visited[nx, ny, Array.IndexOf(directions, d)] = true;
-                        if (distance[nx, ny] > distance[x, y] + 1) distance[nx, ny] = distance[x, y] + 1;
-                        parent[nx, ny] = (x, y);
-                        queue.Enqueue(((nx, ny), d));
-                    }
-                }
-            }
-
-            return distance[end.x, end.y];
-        }
-
-        static void PrintPath((int, int)[,] parent, int endX, int endY)
-        {
-            List<string> path = new List<string>();
-            (int, int) current = (endX, endY);
-
-            while (current != (-1,-1))
-            {
-                path.Add($"({current.Item1}, {current.Item2})");
-                current = parent[current.Item1, current.Item2];
-            }
-
-            path.Reverse();
-            Console.WriteLine("Shortest Path: ");
-            foreach (var step in path)
-            {
-                Console.WriteLine(step);
-            }
-        }
     }
 }
